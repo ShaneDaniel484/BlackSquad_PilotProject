bc1a7d653d7f761647584b999d14ebc189dbc17e
keerthiprasath3801 <113460067+keerthiprasath3801@users.noreply.github.com>
2023-11-16 19:25:18 +0530

    holiday feature

---
+++ src/test/java/com/runner/runnerHoliday.java
@@ -0,0 +1,11 @@
+package com.runner;
+ 
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src\\test\\resources\\features\\holiday.feature",glue ="com.stepDefinitions",plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class runnerHoliday {
+ 
+}
