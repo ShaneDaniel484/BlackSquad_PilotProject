4ee28b2927e321bb3fad3c6931e6b20eb4975533
ShaneDaniel484 <103937168+ShaneDaniel484@users.noreply.github.com>
2023-11-16 19:14:49 +0530

    conflict resolving at baseClass

--- src/test/java/com/utilityClass/BaseClass.java
+++ src/test/java/com/utilityClass/BaseClass.java
@@ -1,30 +1,29 @@
 package com.utilityClass;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseClass {
 	private static BaseClass baseclass;
 	private static WebDriver driver;
-	private static WebDriverWait wait;
 	public final static int TIMEOUT =10;
 	
-	BaseClass(){
+	public BaseClass(){
 		driver = new EdgeDriver();
 		new WebDriverWait(driver,Duration.ofSeconds(TIMEOUT));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
 		driver.manage().window().maximize();
 	}
 	public static void openPage(String url) { 
-		driver.get("https://www.thomascook.in/");
+		driver.get(url);
 	}
 	
 	public static WebDriver getDriver() {
 		return driver;
-	}
+	} 
 	
 	public static void setUpDriver() {
 		if (baseclass==null) {
@@ -40,3 +39,5 @@ public class BaseClass {
 	 baseclass =null;
 	}
 }
+
+
