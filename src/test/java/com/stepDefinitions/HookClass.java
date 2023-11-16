5cf0ad0a4d5a65162437d5e092eaf2ce3e98cbe2
Karthickraja Muralidharan <karthickraja.muralidharan@expleogroup.com>
2023-11-16 19:01:01 +0530

    login feature

---
+++ src/test/java/com/stepDefinitions/HookClass.java
@@ -0,0 +1,19 @@
+package com.stepDefinitions;
+
+import com.utilityClass.BaseClass;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+public class HookClass {
+	
+	@Before
+	public static void setup() {
+		BaseClass.setUpDriver();
+	}
+	
+	@After
+	public static void tearDown(Scenario scenario) {
+		BaseClass.tearDown();
+	}
+}
